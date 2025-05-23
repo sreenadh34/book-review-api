b5018391c8af12894aef4c8cc46145f79d82b9f4
sreenadh34 <43.sreenadh@gmail.com>
2025-05-24 02:52:54 +0530

    feat: add prettier as a dev dependency and update route paths in app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-import express from "express";
-import connectDB from "./config/db.js";
-import cors from "cors";
-import dotenv from "dotenv"; 
+import express from 'express';
+import connectDB from './config/db.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
 import bookRoutes from './routes/book.route.js';
 import reviewRoutes from './routes/review.routes.js';
 import { errorHandler } from './middlewares/error.js';
-
 
 dotenv.config();
 
@@ -20,15 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 // Routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/books', bookRoutes);
-app.use('/api/v1', reviewRoutes);
+app.use('/auth', authRoutes);
+app.use('/books', bookRoutes);
+app.use('/', bookRoutes);
 
 // Error handling
 app.use(errorHandler);
 
-
-const PORT =process.env.PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
